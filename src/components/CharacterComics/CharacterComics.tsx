e19b140f38a5ecf80de34c4572f4d4ce57cce570
Nikita <lubimov.nikita@gmail.com>
2021-10-27 21:25:53 +0300

    inmpelement hooks

--- src/components/CharacterComics/CharacterComics.tsx
+++ src/components/CharacterComics/CharacterComics.tsx
@@ -1,63 +1,32 @@
-import React from 'react';
-import { RouteComponentProps } from 'react-router';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
 import Header from '../Header/Header';
 import Container from '@material-ui/core/Container';
 import './CharacterComics.css';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ComicsList from '../ComicsList/ComicsList';
 import { loadComics } from '../../redux/actions/comicsActions';
-import { IComics } from '../../interfaces';
 import { IRootState } from '../../redux/reducers/index';
-import { Dispatch } from 'redux';
 
 interface ICharacterId {
   characterId: string;
 }
 
-interface ICharacterComicsProps extends RouteComponentProps<ICharacterId> {
-  comics: Array<IComics>;
-  loading: boolean;
-  error: null | string;
-  loadComics: (characterId: string) => void;
+function CharacterComics(): JSX.Element {
+  const { comics, loading } = useSelector((state: IRootState) => state.comicsReducer);
+  const dispatch = useDispatch();
+  const match: ICharacterId = useParams();
+
+  useEffect(() => {
+    dispatch(loadComics(match.characterId));
+  }, []);
+
+  return (
+    <Container className="container">
+      <Header />
+      <ComicsList loading={loading} comics={comics} />
+    </Container>
+  );
 }
 
-interface ICharacterComicsState {
-  characterId: string;
-}
-
-class CharacterComics extends React.Component<ICharacterComicsProps, ICharacterComicsState> {
-  constructor(props: ICharacterComicsProps) {
-    super(props);
-  }
-
-  componentDidMount(): void {
-    this.props.loadComics(this.props.match.params.characterId);
-  }
-
-  render(): JSX.Element {
-    const { comics, loading } = this.props;
-
-    return (
-      <Container className="container">
-        <Header />
-        <ComicsList loading={loading} comics={comics} />
-      </Container>
-    );
-  }
-}
-
-const mapStateToProps = (state: IRootState) => {
-  return {
-    comics: state.comicsReducer.comics,
-    loading: state.comicsReducer.loading,
-    error: state.comicsReducer.error,
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    loadComics: (characterId: string) => dispatch(loadComics(characterId)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(CharacterComics);
+export default CharacterComics;
--- src/components/HomeSearch/HomeSearch.tsx
+++ src/components/HomeSearch/HomeSearch.tsx
@@ -1,111 +1,87 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from '../Header/Header';
 import CharacterList from '../CharacterList/CharacterList';
-import { RouteComponentProps } from 'react-router';
+import { useHistory, useLocation } from 'react-router';
 import './HomeSearch.css';
 import Container from '@material-ui/core/Container';
 import { Spinner } from '../Spinner/Spinner';
 import { SearchBar } from '../SearchBar/SearchBar';
-import { connect } from 'react-redux';
-import { Dispatch } from 'redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { loadCharacters } from '../../redux/actions/charactersActions';
-import { ICharacter } from '../../interfaces';
 import { IRootState } from '../../redux/reducers/index';
 
-interface IHomeSearchProps extends RouteComponentProps {
-  characters: Array<ICharacter>;
-  loading: boolean;
-  error: null | string;
-  loadCharacters: (characterQueryName?: string) => void;
+interface IValue {
+  val: string;
 }
 
-interface IHomeSearchState {
-  inputValue: string;
-}
+function Home(): JSX.Element {
+  const [inputValue, setInputValue] = useState({
+    val: '',
+  });
+  const location = useLocation();
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const { characters, loading, error } = useSelector((state: IRootState) => state.charactersReducer);
 
-class Home extends React.Component<IHomeSearchProps, IHomeSearchState> {
-  constructor(props: IHomeSearchProps) {
-    super(props);
-    this.state = {
-      inputValue: '',
-    };
-    this.setAddress = this.setAddress.bind(this);
-    this.search = this.search.bind(this);
-  }
-
-  getChar(): void {
-    const characterQueryName = new URLSearchParams(this.props.location.search).get('name');
+  function getChar(): void {
+    const characterQueryName = new URLSearchParams(location.search).get('name');
     if (characterQueryName) {
-      this.props.loadCharacters(characterQueryName);
-      this.setState({
-        inputValue: characterQueryName,
+      dispatch(loadCharacters(characterQueryName));
+      setInputValue((prev): IValue => {
+        return {
+          ...prev,
+          val: characterQueryName,
+        };
       });
     } else {
-      this.props.loadCharacters();
-      this.setState({
-        inputValue: '',
+      dispatch(loadCharacters());
+      setInputValue((prev): IValue => {
+        return {
+          ...prev,
+          val: '',
+        };
       });
     }
   }
 
-  componentDidMount(): void {
-    this.getChar();
-  }
+  useEffect(() => {
+    getChar();
+  }, [location.search]);
 
-  componentDidUpdate(prevProps: RouteComponentProps): void {
-    if (this.props.location !== prevProps.location) {
-      this.getChar();
-    }
-  }
-
-  setAddress(event: React.SyntheticEvent): void {
+  function setAddress(event: React.SyntheticEvent): void {
     const { value } = event.target as HTMLInputElement;
-    this.setState({
-      inputValue: value,
+
+    setInputValue((prev): IValue => {
+      return {
+        ...prev,
+        val: value,
+      };
     });
   }
 
-  search(): void {
-    if (this.state.inputValue) {
-      this.props.history.push(`?name=${this.state.inputValue}`);
+  function search(): void {
+    if (inputValue.val) {
+      history.push(`?name=${inputValue.val}`);
     } else {
-      this.props.history.push('/');
+      history.push('/');
     }
   }
 
-  displayError() {
-    if (this.props.error) {
-      return <p className="search-error">{this.props.error}</p>;
+  function displayError() {
+    if (error) {
+      return <p className="search-error">{error}</p>;
     }
   }
 
-  render(): JSX.Element {
-    const { characters, loading, error } = this.props;
-
-    return (
-      <Container className="container">
-        <Header />
-        <SearchBar value={this.state.inputValue} click={this.search} onChange={this.setAddress} />
-        {this.displayError()}
-        {loading ? <Spinner /> : null}
-        <CharacterList items={characters} error={error} />
-      </Container>
-    );
-  }
+  return (
+    <Container className="container">
+      <Header />
+      <SearchBar value={inputValue.val} click={search} onChange={setAddress} />
+      {displayError()}
+      {loading ? <Spinner /> : null}
+      <CharacterList items={characters} error={error} />
+    </Container>
+  );
 }
 
-const mapStateToProps = (state: IRootState) => {
-  return {
-    characters: state.charactersReducer.characters,
-    loading: state.charactersReducer.loading,
-    error: state.charactersReducer.error,
-  };
-};
-
-const mapDispatchToProps = (dispatch: Dispatch) => {
-  return {
-    loadCharacters: (characterQueryName: string) => dispatch(loadCharacters(characterQueryName)),
-  };
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home as any);
+export default Home;
